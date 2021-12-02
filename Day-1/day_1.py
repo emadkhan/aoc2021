1210c2fdd7281071cc32be52e395c3b5d7306cee
Emad Khan <contactemadkhan@gmail.com>
2021-12-02 12:12:56 -0800

    Day1: A little guilty overengineering to make the sliding window an input to the function

--- Day-1/day_1.py
+++ Day-1/day_1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+SLIDING_WINDOW_SIZE = 3
+
 def count_increases(readings):
 	if len(readings) < 2:
 		return 0
@@ -15,15 +17,19 @@ def count_increases(readings):
 	return increases_count	
 
 
-def count_sliding_window_increases(readings):
-	if len(readings) < 4:
+def count_sliding_window_increases(readings, window_size):
+	if len(readings) < window_size + 1:
 		return 0
 
-	prev_sum = readings[0] + readings[1] + readings[2]
+	prev_sum = 0 
+	for i in range(0, window_size):
+		prev_sum += readings[i] 
 	increases_count = 0
 
-	for i in range(1, len(readings) - 2):
-		curr_sum = readings[i] + readings[i + 1] + readings[i + 2]
+	for i in range(1, len(readings) - window_size + 1):
+		curr_sum = 0 
+		for j in range(0, window_size):
+			curr_sum += readings[i + j] 
 		if curr_sum > prev_sum:
 			increases_count += 1
 		prev_sum = curr_sum
@@ -35,7 +41,7 @@ def count_sliding_window_increases(readings):
 if __name__ == "__main__":
 	f = open('input.txt', 'r')
 	readings = [ int(x) for x in f.read().split() ]
-	print("Part 1:{0}, Part 2:{1}".format(count_increases(readings), count_sliding_window_increases(readings)))
+	print("Part 1:{0}, Part 2:{1}".format(count_increases(readings), count_sliding_window_increases(readings, SLIDING_WINDOW_SIZE)))
 
 
 
--- Day-1/day_1_test.py
+++ Day-1/day_1_test.py
@@ -1,4 +1,6 @@
-from day_1 import count_increases, count_sliding_window_increases
+#!/usr/bin/python3
+
+from day_1 import * 
 import unittest
 
 class TestSum(unittest.TestCase):
@@ -13,13 +15,13 @@ class TestSum(unittest.TestCase):
 		self.assertEqual(count_increases([199,200,208,210,200,207,240,269,260,263]), 7, "Should count increases")
 	
 	def test_sliding_window_empty(self):
-		self.assertEqual(count_sliding_window_increases([]), 0, "Should be 0 for empty list")
+		self.assertEqual(count_sliding_window_increases([], SLIDING_WINDOW_SIZE), 0, "Should be 0 for empty list")
 
 	def test_sliding_window_no_increases(self):
-		self.assertEqual(count_sliding_window_increases([5,5,5]), 0, "Should be 0 for too small an input list")
+		self.assertEqual(count_sliding_window_increases([5,5,5], SLIDING_WINDOW_SIZE), 0, "Should be 0 for too small an input list")
 	
 	def test_sliding_window_increases(self):
-		self.assertEqual(count_sliding_window_increases([199,200,208,210,200,207,240,269,260,263]), 5, "Should count increases")
+		self.assertEqual(count_sliding_window_increases([199,200,208,210,200,207,240,269,260,263], SLIDING_WINDOW_SIZE), 5, "Should count increases")
 
 if __name__ == '__main__':
 	unittest.main()
