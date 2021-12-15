c1a90c3d6ccefb426a58ce7ada52083444ffc236
Emad Khan <contactemadkhan@gmail.com>
2021-12-15 14:37:41 -0800

    Day 15 Part 2 working

--- Day-15/day15.py
+++ Day-15/day15.py
@@ -1,31 +1,10 @@
 #!/usr/bin/python3
 
-"""
+from queue import PriorityQueue 
 
-Notes:
-	* Input - Parse into a 2d array of rows and cols. Each entry is >= 1 and <= 9.
-	* Rule - Only count an element if its entered. Starting position does not count as you begin there.
-	* Rule - Start in the top left
-	* Rule - Cannot move diagonally
-	* Output - Find path with the lowest total sum from Top Left to Bottom Right.
-
-Insights:
-	* We only need the total for the result, we do not need to know what the path we take is
-	* We do not want to reenter a position as that is duplication wasteful
-	* You have to watch out for making a locally optimal but globally sub-optimal decision.
-	* If I remember correctly, the bottoms up dynamic programming approach builds the sum at each position
-		 and progresses towards the target. So [0,0] would be 0 [0,1] would be 2, then [0, 2] would be 8 and
-		and [0, 3] would be 11.	
-	* For Top Down dynamic programming, the decisions are:
-		* go right
-		* go left
-		* go up
-		* go down
-"""
-
-MAX = 10
-
-def solve_bottoms_up(input):
+# This is actually incorrect. Works for part 1 but doesn't work for part 1
+# Part 1 gets luck with only going right and down.
+def solve_part_one(input):
 	traversed = [[0] * len(input[0]) for x in range(0, len(input))]
 	for i in range(0, len(input)):
 		for j in range(0, len(input[i])):
@@ -39,25 +18,53 @@ def solve_bottoms_up(input):
 				traversed[i][j] = input[i][j] + min(traversed[i - 1][j], traversed[i][j - 1])
 	return traversed[-1][-1]
 
-# TODO: This doesn't work. Why?
-def solve_top_down(input, i, j, traversed):
-	if i == -1 or j == -1 or i == len(input) or j == len(input[i]):
-		return MAX
-	if i == len(input) - 1 and j == len(input[i]) - 1:
-		print(">>>>>>>>>>>>>>>>> END >>>>>>>>>>>>")
-		return input[i][j]
-	if (i, j) in traversed:
-		return MAX
-	traversed.append((i, j))
-	go_right = solve(input, i, j + 1, traversed)
-	go_left = solve(input, i, j - 1, traversed)
-	go_up = solve(input, i - 1, j, traversed)
-	go_down = solve(input, i + 1, j, traversed)
-	print(traversed)	
-	total = input[i][j] + min(go_right, go_left, go_up, go_down)
-	traversed.pop()
-	return total
+def solve(input):
+	pq = PriorityQueue()
+	pq.put((0, (0,0)))
+	visited = {(0,0)}
+	while pq:
+		risk, (i, j) = pq.get()
+		if i == len(input) - 1 and j == len(input[0]) - 1:
+			return risk
+		neighbours = get_neighbours(input, i, j)
+		for neighbour in neighbours:
+			if neighbour not in visited:
+				pq.put((risk + input[neighbour[0]][neighbour[1]], (neighbour[0], neighbour[1])))
+				visited.add(neighbour)
+	
+
+def get_neighbours(input, i, j):
+	result = []
+	if i > 1:
+		result.append((i - 1, j))
+	if j > 1:
+		result.append((i, j - 1))
+	if i < len(input) - 1:
+		result.append((i + 1, j))
+	if j < len(input[i]) - 1:
+		result.append((i, j + 1))
+	return result
 		
+
+def expand_input(input):
+	result = []
+	# Build Columns
+	for i in range(0, 5):
+		to_add = [[y + i if y + i < 10 else (y + i) % 9 for y in x] for x in input]
+		for row in to_add:
+			result.append(row)
+	base_tile = [x.copy() for x in result]
+	for idx in range(0, len(base_tile)):
+		row = base_tile[idx]
+		for i in range(1, 5):
+			#print("Row: {}".format(row))
+			extend_with = [x + i if x + i < 10 else (x + i) % 9 for x in row]
+
+			#print("Extending with: {}".format(extend_with))
+			result[idx].extend(extend_with)
+
+	return result
+
 def parse_input(filename):
 	f = open(filename)
 	input = f.read().splitlines()
@@ -69,7 +76,6 @@ def parse_input(filename):
 
 if __name__ == '__main__':
 	#input = parse_input('test_input.txt')
-	input = parse_input('larger_input.txt')
+	input = parse_input('input.txt')
 	print("Running")
-	print(solve_bottoms_up(input))
-	
+	print(solve(expand_input(input)))
